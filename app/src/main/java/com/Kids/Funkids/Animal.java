e0d5a2c4448f2004b60d871c1367fe8579de94ae
RanaAdrees <bsef19m026@pucit.edu.pk>
2022-05-21 10:31:08 +0500

    Adding onClick method on each button indivisually and also adding animalFunc on java file

--- app/src/main/java/com/Kids/Funkids/Animal.java
+++ app/src/main/java/com/Kids/Funkids/Animal.java
@@ -2,13 +2,25 @@ package com.Kids.Funkids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Animal extends AppCompatActivity {
+    ImageView img;
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal);
+        img=findViewById(R.id.animalShow);
+        name=findViewById(R.id.animalName);
+
+        Intent intent=getIntent();
+//      img.setImageResource(R.drawable.tiger);
+        name.setText(intent.getStringExtra("animalName"));
     }
 }
--- app/src/main/java/com/Kids/Funkids/ListOfAnimals.java
+++ app/src/main/java/com/Kids/Funkids/ListOfAnimals.java
@@ -2,15 +2,28 @@ package com.Kids.Funkids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class ListOfAnimals extends AppCompatActivity {
-    ImageButton animalName;
+
+    public void animalFunc(View view)
+    {
+        int id=view.getId();
+        String name=view.getResources().getResourceEntryName(id);
+        Intent intent=new Intent(this,Animal.class);
+        intent.putExtra("animalName",name);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_animals);
     }
+
 }
