f2f7d2949dbae12d00e9f98bb9d973d3f256c4a1
RanaAdrees <bsef19m026@pucit.edu.pk>
2022-06-05 13:42:13 +0500

    code 'getView' method in "CustomBaseAdapter" class

--- app/src/main/java/com/Kids/Funkids/CustomBaseAdapter.java
+++ app/src/main/java/com/Kids/Funkids/CustomBaseAdapter.java
@@ -1,15 +1,31 @@
 package com.Kids.Funkids;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CustomBaseAdapter extends BaseAdapter {
 
+    Context context;
+     String animalList[];
+     int animalImages[];
+     LayoutInflater inflater;
+
+     public CustomBaseAdapter(Context ctx,String animalList[],int animalImages[]){
+         this.context=ctx;
+         this.animalList=animalList;
+         this.animalImages=animalImages;
+         inflater=LayoutInflater.from(ctx);
+
+     }
     @Override
     public int getCount() {
-        return 0;
+        return animalList.length;
     }
 
     @Override
@@ -23,7 +39,13 @@ public class CustomBaseAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View view, ViewGroup parent) {
+         view=inflater.inflate(R.layout.activity_custom_list_view,null);
+        TextView txtView=view.findViewById(R.id.ItemName);
+        ImageView imgView=view.findViewById(R.id.ImageIcon);
+
+        txtView.setText(animalList[position]);
+        imgView.setImageResource(animalImages[position]);
+        return view;
     }
 }
